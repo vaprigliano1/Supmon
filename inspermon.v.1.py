914d46d70edb099547767b9f8d1d3232b0fdf417
nicolasbss <nicolasbs@al.insper.edu.br>
2017-03-30 19:15:38 -0300

    Delete inspermon.v.1.py

--- inspermon.v.1.py
+++
@@ -1,2 +0,0 @@
-Insperdex = {"Techmon" : {"Ataque" : 70, "Defesa": 50, "PV" : 120}, "Weirdomon" : {"Ataque" : 40, "Defesa": 80, "PV" : 110}, "Pythonbat": {"Ataque": 60, "Defesa" : 60, "PV":140}, "Poliswag": {"Ataque" : 75, "Defesa": 40, "PV" : 120}, "Charmano":{"Ataque":50, "Defesa":60, "PV":180}}
-print(Insperdex["Techmon"]["Ataque"])
