ad701ce52776a821ad027ad97c245734a63f128b
nicolasbss <nicolasbs@al.insper.edu.br>
2017-04-03 19:24:29 -0300

    Add files via upload

--- ep2.py
+++ ep2.py
@@ -13,21 +13,23 @@ def padroniza(resposta):
 
 def batalha(inspermon1, inspermon2):
 	Insperdex = {"Techmon" : {"Ataque" : 170, "Defesa": 50, "PV" : 120}, "Weirdomon" : {"Ataque" : 140, "Defesa": 80, "PV" : 110}, "Pythonbat": {"Ataque": 160, "Defesa" : 60, "PV":140}, "Poliswag": {"Ataque" : 175, "Defesa": 40, "PV" : 120}, "Charmano":{"Ataque":150, "Defesa":60, "PV":180}}
-	resultado1 = Insperdex[inspermon2]["PV"]
-	resultado2 = Insperdex[inspermon1]["PV"]
-	while True:
+	resultado1 = [Insperdex[inspermon2]["PV"]]
+	resultado2 = [Insperdex[inspermon1]["PV"]]
+	for i in range (1,1000):
 		dano1 = Insperdex[inspermon1]["Ataque"] - Insperdex[inspermon2]["Defesa"]
-		dano2 = Insperdex[inspermon2]["Ataque"] - Insperdex[inspermon1]["Defesa"]
-		resultado1 = resultado1 - dano1
-		resultado2 = resultado2 - dano2
 		print("É sua vez de atacar, concentre-se!")
+		dano2 = Insperdex[inspermon2]["Ataque"] - Insperdex[inspermon1]["Defesa"]
 		atacar = input("Para atacar digite ataque:") #adicionar opção com x% de chance de desistir da batalha no meio da luta.
 		atacar = padroniza(atacar)
+		resultado1 = resultado1 + [resultado1[i-1] - dano1]
+		resultado2 = resultado2 + [resultado2[i-1] - dano2]
 		if atacar == "Ataque":
 			print("Seu Inspermon está atacando")
 			time.sleep(2)
-			print("Os pontos de vida de seu inimigo foram de {0} para {1}".format(Insperdex[monaparece]["PV"], resultado1))
-			if resultado1 <= 0:
+			if resultado1[i] > 0:
+				print("Os pontos de vida de seu inimigo foram de {0} para {1}".format(resultado1[i-1], resultado1[i]))
+			if resultado1[i] <= 0:
+				print("Os pontos de vida de seu inimigo foram de {0} para 0".format(resultado1[i-1]))
 				print("{0} desmaiou, {1} é o vencedor da batalha!".format(inspermon2, inspermon1))
 				break
 		time.sleep(2)
@@ -35,11 +37,13 @@ def batalha(inspermon1, inspermon2):
 		time.sleep(2)
 		print("...")
 		time.sleep(1)
-		print("Seus pontos de vida foram de {0} para {1}".format(Insperdex[inspermon1]["PV"], resultado2))
-		if resultado2 <= 0:
+		if resultado2[i] > 0:
+			print("Seus pontos de vida foram de {0} para {1}".format(resultado2[i-1], resultado2[i]))
+		else:
+			print("Seus pontos de vida foram de {0} para 0".format(resultado2[i-1]))
 			print ("Seu inspermon desmaiou, que pena... Cure-o em um centro Inspermon mais proximo e tente novamente")
 			break
-	
+	return print("Continuando aventura, a vida de seu inspermon foi regenerada")
 
 #ecolhendo seu inspermon inicial
 inspermon_inicial = input("qual será seu pokemon inicial, Techmon, Weirdomon, Pythonbat, Poliswag ou Charmano? ")
