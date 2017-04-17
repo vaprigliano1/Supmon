1c891ccd72b1d3f523886044358baeeb12d3b639
nicolasbss <nicolasbs@al.insper.edu.br>
2017-04-17 20:01:04 -0300

    Add files via upload
    adiconado no arquivo principal as probabilidades de fuga

--- INSPERMON.py
+++ INSPERMON.py
@@ -3,16 +3,10 @@ import time
 
 #definindo a classe Inspermon
 class Inspermon():
-	def __init__(self, ataque, defesa, velocidade, vida):
+	def __init__(self, ataque, defesa, vida):
 		self.a = ataque
 		self.d = defesa
-		self.v = velocidade
 		self.pv = vida
-
-Insperdex = {"Techmon" : {"Ataque" : 170, "Defesa": 50, "PV" : 120}, "Weirdomon" : {"Ataque" : 140, "Defesa": 80, "PV" : 110}, "Pythonbat": {"Ataque": 160, "Defesa" : 60, "PV":140}, "Poliswag": {"Ataque" : 175, "Defesa": 40, "PV" : 120}, "Charmano":{"Ataque":150, "Defesa":60, "PV":180}}
-listainspermon = ["Techmon", "Weirdomon","Pythonbat","Poliswag","Charmano"]
-
-
 
 #definindo a função que padronizará a resposta
 def padroniza(resposta):
@@ -21,58 +15,79 @@ def padroniza(resposta):
 	resposta = resposta.title()
 	return resposta
 
+#definindo a função de batalhas
 def batalha(inspermon1, inspermon2):
-	resultado1 = Insperdex[inspermon2]["PV"]
-	resultado2 = Insperdex[inspermon1]["PV"]
-	while True:
-		dano1 = Insperdex[inspermon1]["Ataque"] - Insperdex[inspermon2]["Defesa"]
-		dano2 = Insperdex[inspermon2]["Ataque"] - Insperdex[inspermon1]["Defesa"]
-		resultado1 = resultado1 - dano1
-		resultado2 = resultado2 - dano2
-		print("É sua vez de atacar, quer continuar ou prefere desistir da batalha?")
-		ação = input("Para atacar digite 'ataque', para fugir digite 'fuga':") 
-		ação = padroniza(ação)
-		if ação == "Ataque":
+	resultado1 = [inspermon2.pv]
+	resultado2 = [inspermon1.pv]
+	for i in range (1, 1000):
+		dano1 = inspermon1.a - inspermon2.d		
+		dano2 = inspermon2.a - inspermon1.d
+		print("É o seu turno, concentre-se!")
+		ação = input("Para atacar digite 'ataque', mas se você quer tentar fugir, digite 'fuja'")
+		ação = padroniza(atacar)
+		resultado1 = resultado1 + [resultado1[i-1] - dano1]
+		resultado2 = resultado2 + [resultado2[i-1] - dano2]
+		if atacar == "Ataque":
 			print("Seu Inspermon está atacando")
 			time.sleep(2)
-			print("Os pontos de vida de seu inimigo foram de {0} para {1}".format(Insperdex[monaparece]["PV"], resultado1))
-			if resultado1 <= 0:
+			if resultado1[i] > 0:
+				print("Os pontos de vida de seu inimigo foram de {0} para {1}".format(resultado1[i-1], resultado1[i]))
+			if resultado1[i] <= 0:
+				print("Os pontos de vida de seu inimigo foram de {0} para 0".format(resultado1[i-1]))
 				print("{0} desmaiou, {1} é o vencedor da batalha!".format(inspermon2, inspermon1))
+				print("Continuando aventura, a vida de seu inspermon foi regenerada")
 				break
-		if ação == "Fuga":
-			print ("OK! Vamos tentar fugir...")
-			time.sleep (2)
-			desistencia = ["1","2","3"] #PROBABILIDADE DE FUGIR DA BATALHA.
-			probD = random.choice(desistencia)
-			if probD == 1 or 2 :
-				print("Fugiu com exito!")
-			break
-			if probD == 3 :
-				print ("Você não conseguiu fugir, vamos continuar a batalha!")
-				print (batalha(inspermon_inicial, monaparece))
 		time.sleep(2)
 		print("Agora seu inimigo atacará, se prepare!")
 		time.sleep(2)
 		print("...")
 		time.sleep(1)
-		print("Seus pontos de vida foram de {0} para {1}".format(Insperdex[inspermon1]["PV"], resultado2))
-		if resultado2 <= 0:
+		if resultado2[i] > 0:
+			print("Seus pontos de vida foram de {0} para {1}".format(resultado2[i-1], resultado2[i]))
+		else:
+			print("Seus pontos de vida foram de {0} para 0".format(resultado2[i-1]))
 			print ("Seu inspermon desmaiou, que pena... Cure-o em um centro Inspermon mais proximo e tente novamente")
 			break
-		
+		if ação == "Fuga":
+			print ("OK! Vamos tentar fugir...")
+			time.sleep (2)
+			#criando uma probabilidade de fuga da batalha
+			desistencia = ["1","2","3"]
+			probD = random.choice(desistencia)
+			if probD == 1 or 2 :
+				print("Fugiu com exito!")
+				break
+			if probD == 3 :
+				print ("Você não conseguiu fugir, vamos continuar a batalha!")
+				pass
+			
+
+
+#criando os inspermons
+Techmon = Inspermon(170, 50, 120)
+Weirdomon = Inspermon(140, 80, 110)
+Pythonbat = Inspermon(160, 60, 140)
+Poliswag = Inspermon(175, 40, 120)
+Charmano = Inspermon(150, 60, 180)
+
+#lista de inspermons
+listainspermon = [Techmon, Weirdomon, Pythonbat, Poliswag, Charmano]
+
+#criando um dicionário de inspermons
+base_de_dados_inspermon = {Techmon: {"Ataque": Techmon.a, "Defesa": Techmon.d, "Pontos de vida": Techmon.pv}, Weirdomon: {"Ataque": Weirdomon.a, "Defesa": Weirdomon.d, "Pontos de vida": Weirdomon.pv}, Pythonbat: {"Ataque": Pythonbat.a, "Defesa": Pythonbat.d, "Pontos de vida": Pythonbat.pv}, Poliswag: {"Ataque": Poliswag.a, "Defesa": Poliswag.d, "Pontos de vida": Poliswag.pv}, Charmano: {"Ataque": Charmano.a, "Defesa": Charmano.d, "Pontos de vida": Charmano.pv}}
 
 #apresentando o mundo de inspermun
-print ("Bem vindo ao mundo de Inspermon, um mundo cheio de Inspermons a serem descobertos")
+print ("Bem vindo ao mundo de Inspermon, um mundo cheio de inspermons desesperados para te destruir")
 time.sleep(1)
 nome_do_jogador = input("Parabéns por iniciar sua aventura, qual é o seu nome?")
-padroniza(nome_do_jogador)
+nome_do_jogador = padroniza(nome_do_jogador)
 time.sleep(1)
 	
 
 #ecolhendo seu inspermon inicial
-inspermon_inicial = input("qual será seu pokemon inicial, Techmon, Weirdomon, Pythonbat, Poliswag ou Charmano? ")
+inspermon_inicial = input("qual será seu pokemon inicial, Techmon, Weirdomon, Pythonbat, Poliswag ou Charmano?")
 inspermon_inicial = padroniza(inspermon_inicial)
-if inspermon_inicial in Insperdex:
+if inspermon_inicial in listainspermon:
 	print("parabéns {0}, {1} é uma boa escolha".format(nome_do_jogador, inspermon_inicial))
 
 #iniciando a aventura
@@ -85,14 +100,15 @@ while True:
 	if inicio == "Passear":
 		print ("...")
 		time.sleep (2)
-		monaparece = random.choice(listainspermon)#definindo qual inspermon aparecerá
-		if monaparece in Insperdex.keys():
-			print("A wild {0} aparece, seus atributos são {1}".format(monaparece, Insperdex[monaparece]))
-			atributos = Insperdex[monaparece]
-			ação = input("O que você quer fazer, batalhar ou fugir? ")
+		#definindo qual inspermon aparecerá
+		inspermon_aparece = random.choice(listainspermon)
+		if inspermon_aparece in listainspermon:
+			print("A wild {0} aparece, seus atributos são: {1}, enquanto os de seu inspermon sâo {2}".format(inspermon_aparece, base_de_dados_inspermon[inspermon_aparece], base_de_dados_inspermon[inspermon_inicial]))
+			ação = input("O que você quer fazer, batalhar ou fugir?")
 			ação = padroniza(ação)
 	if ação == "Fugir":
-		desistencia = ["1","2","3"] #PROBABILIDADE DE FUGIR DA BATALHA.
+		#adicionando probabilidade de fuga na batalha
+		desistencia = ["1","2","3"]
 		probD = random.choice(desistencia)
 		if probD == 1 or 2:
 			print("Fugiu com exito!")
@@ -103,4 +119,4 @@ while True:
 		
 	if ação == "Batalhar":
 		print("Iniciando batalha, may God have mercy on your soul")
-		print(batalha(inspermon_inicial, monaparece))
+		print(batalha(inspermon_inicial, inspermon_aparece))
