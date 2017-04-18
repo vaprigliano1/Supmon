12db783931443eabf9c472a0677f7af514bf375a
nicolasbss <nicolasbs@al.insper.edu.br>
2017-04-18 20:24:54 -0300

    Add files via upload
    adicionado o Insperdex

--- INSPERMON.py
+++ INSPERMON.py
@@ -2,6 +2,9 @@ import random
 import time
 from funcoes import *
 
+#criando o dicionário que virará sua Insperdex
+Insperdex = {}
+
 #criando os inspermons
 Techmon = Inspermon("Techmon", 170, 50, 120)
 Weirdomon = Inspermon("Weirdomon", 140, 80, 110)
@@ -10,7 +13,7 @@ Poliswag = Inspermon("Poliswag", 175, 40, 120)
 Charmano = Inspermon("Charmano", 150, 60, 180)
 
 
-#criando uma base de dados com os atributos dos pokemons
+#criando uma base de dados com os atributos dos inspermons
 bd = {"Techmon": Techmon, "Weirdomon": Weirdomon, "Pythonbat": Pythonbat, "Poliswag": Poliswag, "Charmano": Charmano}
 
 #apresentando o mundo de inspermun
@@ -22,17 +25,19 @@ time.sleep(1)
 	
 
 #ecolhendo seu inspermon inicial
-inspermon_inicial = input("qual será seu pokemon inicial, Techmon, Weirdomon, Pythonbat, Poliswag ou Charmano? ")
+inspermon_inicial = input("Olá {0}! qual será seu pokemon inicial, Techmon, Weirdomon, Pythonbat, Poliswag ou Charmano? ".format(nome_do_jogador))
 inspermon_inicial = padroniza(inspermon_inicial)
 if inspermon_inicial in bd.keys():
 	print("parabéns {0}, {1} é uma boa escolha!".format(nome_do_jogador, inspermon_inicial))
+	Insperdex[inspermon_inicial] = {"Ataque": bd[inspermon_inicial].a, "Defesa": bd[inspermon_inicial].d, "Pontos de vida": bd[inspermon_inicial].pv}
 
 #iniciando a aventura
 while True:
 	inicio = input("o que você vai fazer, passear, dormir ou ver sua Insperdex? ")
 	inicio = padroniza(inicio)
 	if inicio == "Insperdex":
-
+		print (Insperdex)
+		pass
 	if inicio == "Dormir":
 		print ("Bom descanso!")
 		break
@@ -42,7 +47,11 @@ while True:
 		#definindo qual inspermon aparecerá
 		inspermon_aparece = random.choice(list(bd.keys()))
 		if inspermon_aparece in bd.keys():
-			print("A wild {0} aparece, seus atributos são: ataque {1}, defesa {2} e pontos de vida {3}, enquanto os de seu inspermon sâo ataque {4}, defesa {5}, pontos de vida {6}".format(inspermon_aparece, bd[inspermon_aparece].a, bd[inspermon_aparece].d, bd[inspermon_aparece].pv, bd[inspermon_inicial].a, bd[inspermon_inicial].d, bd[inspermon_inicial].pv))
+			print("Um {0} apareceu, segundo a Insperdex seus atributos são: ataque {1}, defesa {2} e pontos de vida {3}, enquanto os de seu inspermon sâo ataque {4}, defesa {5}, pontos de vida {6}".format(inspermon_aparece, bd[inspermon_aparece].a, bd[inspermon_aparece].d, bd[inspermon_aparece].pv, bd[inspermon_inicial].a, bd[inspermon_inicial].d, bd[inspermon_inicial].pv))
+			time.sleep(1)
+			if inspermon_aparece not in Insperdex.keys():
+				print("{0} foi adicionado a sua Insperdex".format(inspermon_aparece))
+				Insperdex[inspermon_aparece] = {"Ataque": bd[inspermon_aparece].a, "Defesa": bd[inspermon_aparece].d, "Pontos de vida": bd[inspermon_aparece].pv}
 			ação = input("O que você quer fazer, batalhar ou fugir? ")
 			ação = padroniza(ação)
 		if ação == "Fugir":
