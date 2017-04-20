9b42f241607d30599416c48440eae5749bcf2815
nicolasbss <nicolasbs@al.insper.edu.br>
2017-04-20 20:28:15 -0300

    Add files via upload
    adicionada a evolução e corrigido diversos bugs

--- INSPERMON.py
+++ INSPERMON.py
@@ -5,19 +5,27 @@ from funcoes_e_classes import *
 #criando o dicionário que virará sua Insperdex
 Insperdex = {}
 
-#criando os inspermons
-Techmon = Inspermon("Techmon", 170, 50, 120)
-Weirdomon = Inspermon("Weirdomon", 140, 80, 110)
-Pythonbat = Inspermon("Pythonbat", 160, 60, 140)
-Poliswag = Inspermon("Poliswag", 175, 40, 120)
-Charmano = Inspermon("Charmano", 150, 60, 180)
+#criando os inspermons e suas evoluções
+Techmon = Inspermon(170, 50, 120, 10)
+Mechamon = Inspermon(200, 70, 140, 25)
 
+Weirdomon = Inspermon(140, 80, 110, 15)
+Strangemon = Inspermon(170, 100, 140, 30)
+
+Pythonbat = Inspermon(160, 60, 140, 5)
+Javabat = Inspermon(180, 80, 160, 12)
+
+Poliswag = Inspermon(175, 40, 120, 10)
+Insperswag = Inspermon(190, 50, 140, 15)
+
+Charmano = Inspermon(150, 60, 180, 20)
+Charbrother = Inspermon(200, 90, 190, 30)
 
 #criando uma base de dados com os atributos dos inspermons
-bd = {"Techmon": Techmon, "Weirdomon": Weirdomon, "Pythonbat": Pythonbat, "Poliswag": Poliswag, "Charmano": Charmano}
+bd = {"Techmon": Techmon, "Mechamon": Mechamon, "Weirdomon": Weirdomon, "Strangemon": Strangemon, "Pythonbat": Pythonbat, "Javabat":Javabat, "Poliswag": Poliswag, "Insperswag": Insperswag, "Charmano": Charmano, "Charbrother": Charbrother}
 
 #apresentando o mundo de inspermun
-print ("Bem vindo ao mundo de Inspermon, um mundo cheio criaturas misteriosas, mas tome cuidado! nem todas são doceis!")
+print ("Bem vindo ao mundo de Inspermon, um mundo cheio de inspermons que desejam seu fim")
 time.sleep(1)
 nome_do_jogador = input("Parabéns por iniciar sua aventura, qual é o seu nome? ")
 nome_do_jogador = padroniza(nome_do_jogador)
@@ -25,18 +33,26 @@ time.sleep(1)
 	
 
 #ecolhendo seu inspermon inicial
-inspermon_inicial = input("Olá {0}! qual será seu pokemon inicial, Techmon, Weirdomon, Pythonbat, Poliswag ou Charmano? ".format(nome_do_jogador))
-inspermon_inicial = padroniza(inspermon_inicial)
-if inspermon_inicial in bd.keys():
-	print("parabéns {0}, {1} é uma boa escolha!".format(nome_do_jogador, inspermon_inicial))
-	Insperdex[inspermon_inicial] = {"Ataque": bd[inspermon_inicial].a, "Defesa": bd[inspermon_inicial].d, "Pontos de vida": bd[inspermon_inicial].pv}
+while True:
+	inspermon_inicial = input("Olá {0}! qual será seu pokemon inicial, Techmon, Weirdomon, Pythonbat, Poliswag ou Charmano? ".format(nome_do_jogador))
+	inspermon_inicial = padroniza(inspermon_inicial)
+	if inspermon_inicial in bd.keys():
+		print("parabéns {0}, {1} é uma boa escolha!".format(nome_do_jogador, inspermon_inicial))
+		Insperdex[inspermon_inicial] = {"Ataque": bd[inspermon_inicial].a, "Defesa": bd[inspermon_inicial].d, "Pontos de vida": bd[inspermon_inicial].pv}
+		break
+	else:
+		print ("Não há um pokemon com esse nome disponível, escolha um da lista")
 
 #iniciando a aventura
+inicioesperado = ["Passear", "Dormir", "Insperdex"]
+açãoesperada = ["Batalhar", "Fugir"]
+experiencia_atual = 0
 while True:
-	inicio = input("{0}, o que você quer fazer? Passear com seu Inspermon, dormir ou ver sua Insperdex?".format(nome_do_jogador))
+	inicio = input("o que você vai fazer, passear, dormir ou ver sua Insperdex? ")
 	inicio = padroniza(inicio)
+	if inicio not in inicioesperado:
+		print ("Não entendemos o que você disse")
 	if inicio == "Insperdex":
-		print ("Essa é sua insperdex!")
 		print (Insperdex)
 		pass
 	if inicio == "Dormir":
@@ -48,22 +64,57 @@ while True:
 		#definindo qual inspermon aparecerá
 		inspermon_aparece = random.choice(list(bd.keys()))
 		if inspermon_aparece in bd.keys():
-			print("Um {0} apareceu, seus atributos são: ataque :{1}, defesa :{2} e pontos de vida :{3}, enquanto os de seu Inspermon sâo ataque :{4}, defesa :{5}, pontos de vida :{6}".format(inspermon_aparece, bd[inspermon_aparece].a, bd[inspermon_aparece].d, bd[inspermon_aparece].pv, bd[inspermon_inicial].a, bd[inspermon_inicial].d, bd[inspermon_inicial].pv))
+			print("Um {0} apareceu, segundo a Insperdex seus atributos são: ataque :{1}, defesa :{2} e pontos de vida :{3}, enquanto os de seu inspermon sâo ataque :{4}, defesa :{5}, pontos de vida :{6}".format(inspermon_aparece, bd[inspermon_aparece].a, bd[inspermon_aparece].d, bd[inspermon_aparece].pv, bd[inspermon_inicial].a, bd[inspermon_inicial].d, bd[inspermon_inicial].pv))
 			time.sleep(1)
 			if inspermon_aparece not in Insperdex.keys():
-				print("Você encontrou um novo Inspermon!, {0} foi adicionado a sua Insperdex".format(inspermon_aparece))
+				print("{0} foi adicionado a sua Insperdex".format(inspermon_aparece))
 				Insperdex[inspermon_aparece] = {"Ataque": bd[inspermon_aparece].a, "Defesa": bd[inspermon_aparece].d, "Pontos de vida": bd[inspermon_aparece].pv}
-			ação = input("Treinador Inspermon, você vai batalhar ou fugir? ")
+			ação = input("O que você quer fazer, batalhar ou fugir? ")
 			ação = padroniza(ação)
 		if ação == "Fugir":
-			#adicionando probabilidade de fuga na batalha
+			#adicionando probabilidade de fuga no início da batalha
 			probD = random.randint(0, 5)
 			if probD in range(0, 3):
 				print("Fugiu com exito!")
 				pass
 			else:
 				print ("Você não conseguiu fugir, vamos batalhar!")
-				print (batalha(inspermon_inicial, monaparece, bd))
+				print (batalha(inspermon_inicial, inspermon_aparece, bd, experiencia_atual))
+				experiencia = "nada"
 		if ação == "Batalhar":
 			print("Iniciando batalha, may God have mercy on your soul")
-			print(batalha(inspermon_inicial, inspermon_aparece, bd))
+			experiencia = batalha(inspermon_inicial, inspermon_aparece, bd, experiencia_atual)
+		if ação not in açãoesperada:
+			print (ação)
+			print ("Não entendemos o que você disse")
+			experiencia ="nada"
+			
+		#evoluindo seu inspermon
+		if type(experiencia) == type(0):
+			if experiencia > 1:
+				if inspermon_inicial == "Techmon":
+					print ("Parabéns! seu {0} evoluiu para {1}!".format(inspermon_inicial, "Mechamon"))
+					inspermon_inicial = "Mechamon"
+					time.sleep(2)
+					print ("Seus atributos agora são: ataque:{0}, defesa: {1} e pontos de vida:{2}".format(bd[inspermon_inicial].a, bd[inspermon_inicial].d, bd[inspermon_inicial].pv))
+				if inspermon_inicial == "Weirdomon":
+					print ("Parabéns! seu {0} evoluiu para {1}".format(inspermon_inicial, "Strangemon"))
+					inspermon_inicial = "Strangemon"
+					time.sleep(2)
+					print ("Seus atributos agora são: ataque:{0}, defesa: {1} e pontos de vida:{2}".format(bd[inspermon_inicial].a, bd[inspermon_inicial].d, bd[inspermon_inicial].pv))
+				if inspermon_inicial == "Pythonbat":
+					print ("Parabéns! seu {0} evoluiu para {1}".format(inspermon_inicial, "Javabat"))
+					inspermon_inicial = "Javabat"
+					time.sleep(2)
+					print ("Seus atributos agora são: ataque:{0}, defesa: {1} e pontos de vida:{2}".format(bd[inspermon_inicial].a, bd[inspermon_inicial].d, bd[inspermon_inicial].pv))
+				if inspermon_inicial == "Poliswag":
+					print ("Parabéns! seu {0} evoluiu para {1}".format(inspermon_inicial, "Insperswag"))
+					inspermon_inicial = "Insperswag"
+					time.sleep(2)
+					print ("Seus atributos agora são: ataque:{0}, defesa: {1} e pontos de vida:{2}".format(bd[inspermon_inicial].a, bd[inspermon_inicial].d, bd[inspermon_inicial].pv))
+				if inspermon_inicial == "Charmano":
+					print ("Parabéns! seu {0} evoluiu para {1}".format(inspermon_inicial, "Charbrother"))
+					inspermon_inicial = "Charbrother"
+					time.sleep(2)
+					print ("Seus atributos agora são: ataque:{0}, defesa: {1} e pontos de vida:{2}".format(bd[inspermon_inicial].a, bd[inspermon_inicial].d, bd[inspermon_inicial].pv))
+			experiencia = str(experiencia)
--- funcoes_e_classes.py
+++ funcoes_e_classes.py
@@ -1,41 +1,38 @@
 import random
 import time
 
+respostasesperadas = ["Fuja", "Ataque"]
+
 #definindo a classe inspermon
 class Inspermon():
-	def __init__(self, pknome, ataque, defesa, vida):
+	def __init__(self, ataque, defesa, vida, experiencia):
 		self.a = ataque
 		self.d = defesa
 		self.pv = vida
+		self.exp = experiencia
 
-	def __repr__(self):
-		return "Inspermon()"
-
-	def __str__(self):
-		return self.pknome
 
 #definindo a função que padronizará a resposta
 def padroniza(resposta):
 	resposta = resposta.strip(" ")
-	resposta = resposta.lower()
 	resposta = resposta.title()
 	return resposta
 
-#definindo a função de batalhas
-def batalha(inspermon1, inspermon2, bd):
+
+#definindo a função de batalha
+def batalha(inspermon1, inspermon2, bd, experiencia_atual):
 	resultado1 = [bd[inspermon2].pv]
 	resultado2 = [bd[inspermon1].pv]
 	for i in range (1, 1000):
 		print("É o seu turno, concentre-se!")
-		ação = input("Para atacar digite :'atacar', mas se você quer tentar fugir, digite :'fugir'")
-		ação = padroniza(ação)	
+		batalhando = input("Para atacar digite 'ataque', mas se você quer tentar fugir, digite 'fuja' ")
+		batalhando = padroniza(batalhando)	
 		dano1 = bd[inspermon1].a - bd[inspermon2].d
 		dano2 = bd[inspermon2].a - bd[inspermon1].d
-		if ação == "Atacar":
-			resultado1 = resultado1 + [resultado1[i-1] - dano1]
-			resultado2 = resultado2 + [resultado2[i-1] - dano2]
+		if batalhando == "Ataque":
+			resultado1.append(resultado1[i-1] - dano1)
+			resultado2.append(resultado2[i-1] - dano2)
 			print("Seu Inspermon está atacando")
-			print ("...")
 			time.sleep(2)
 			if resultado1[i] > 0:
 				print("Os pontos de vida de seu inimigo foram de {0} para {1}".format(resultado1[i-1], resultado1[i]))
@@ -43,12 +40,13 @@ def batalha(inspermon1, inspermon2, bd):
 				print("Os pontos de vida de seu inimigo foram de {0} para 0".format(resultado1[i-1]))
 				print("{0} desmaiou, {1} é o vencedor da batalha!".format(inspermon2, inspermon1))
 				sortepv = random.randint(0, 6)
-				if sortepv < 1:
+				if sortepv < 2:
 					print ("Oh não, seu inimigo tinha uma carta na manga! Ele tomou uma poção e seus pontos de vidas foram regenerados em 50!")
 					resultado1[i] = 50
-				else:
+				if sortepv >= 2:
 					print("Continuando aventura, a vida de seu inspermon foi regenerada")
-					break
+					experiencia_nova = experiencia_atual + bd[inspermon2].exp
+					return experiencia_nova
 			time.sleep(2)
 			print("Agora seu inimigo atacará, se prepare!")
 			time.sleep(2)
@@ -56,12 +54,12 @@ def batalha(inspermon1, inspermon2, bd):
 			time.sleep(1)
 			if resultado2[i] > 0:
 				print("Seus pontos de vida foram de {0} para {1}".format(resultado2[i-1], resultado2[i]))
-			else:
+			if resultado2[i] <= 0:
 				print("Seus pontos de vida foram de {0} para 0".format(resultado2[i-1]))
-				print ("Seu inspermon desmaiou, que pena... Cure-o em um centro Inspermon mais proximo e tente novamente")
-				break
+				print ("Seu inspermon desmaiou, que pena... Espere até que ele se regenere")
+				return
 		#criando uma probabilidade de fuga da batalha		
-		if ação == "Fugir":
+		if batalhando == "Fuja":
 			resultado1 = resultado1 + [resultado1[i-1]]
 			resultado2 = resultado2 + [resultado2[i-1]]
 			print ("OK! Vamos tentar fugir...")
@@ -69,7 +67,12 @@ def batalha(inspermon1, inspermon2, bd):
 			probD = random.randint(0, 5)
 			if probD in range(0,3):
 				print("Fugiu com exito!")
-				break
+				return
 			else:
 				print ("Você não conseguiu fugir, vamos continuar a batalha!")
-				pass
+		#caso o jogador escreva algo diferente do esperado
+		if batalhando not in respostasesperadas:
+			resultado1 = resultado1 + [resultado1[i-1]]
+			resultado2 = resultado2 + [resultado2[i-1]]
+			print ("Não entendemos o que você quer fazer.")
+			
