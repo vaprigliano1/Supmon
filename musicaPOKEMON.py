27697bef1e5de4e7f59271ae9b094b7a4fe6aab2
vaprigliano1 <victor.aprigliano1@hotmail.com>
2017-04-20 12:13:01 -0300

    Add files via upload

---
+++ musicaPOKEMON.py
@@ -0,0 +1,10 @@
+
+import pygame
+from pygame.locals import *
+from pygame.mixer import Sound
+pygame.mixer.init()
+pygame.mixer.music.load("po.mp3")
+pygame.mixer.music.play(-1,0.0)
+
+#adicionar restart
+#adicionar stop depois de brake e restart
