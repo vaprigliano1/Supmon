6bbf4cf8f563ea153ca08541012fbf3d1d05695f
vaprigliano1 <victor.aprigliano1@hotmail.com>
2017-04-23 12:29:01 -0300

    Delete musicaPOKEMON.py

--- musicaPOKEMON.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from pygame.locals import *
-from pygame.mixer import Sound
-pygame.init()
-pygame.mixer.music.load('po.mp3') #salvar nome do aquivo com este nome
-pygame.mixer.music.play()
-
-#adicionar restart
-#adicionar stop depois de brake e restart
